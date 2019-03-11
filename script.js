f7c4979b03e0cee18b60be4dfa4d9eb092380cf1
Paulina Oster <paulina.oster@gmail.com>
2019-03-11 16:30:17 +0100

    finished sections: premiere-form, contact, footer and thankyou.html

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.getElementById('submit-contact').onclick=alert('Wiadomość wysłana!');
+// document.getElementById('submit-contact').onclick=alert('Wiadomość wysłana!');
 document.getElementById('hero-button').onclick=document.getElementById('popup-window').style.display='block';
