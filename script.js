09f95aadf15510b8e838e4ae0e6642ce7921442a
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-13 16:52:11 +0100

    Start slider.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// document.getElementById('submit-contact').onclick=alert('Wiadomość wysłana!');
-document.getElementById('hero-button').onclick=document.getElementById('popup-window').style.display='block';
+const activeSlide = document.querySelector('.carousel__slide--active');
+const slides = document.querySelectorAll('.carousel__slide');
+
+
+const changeSlide = setInterval(function() {
+    //slides[i].classList.add('carousel__slide--active');
+
+    activeSlide.classList.remove('carousel__slide--active');
+}, 5000);
+
