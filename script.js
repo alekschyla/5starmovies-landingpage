9b60ec5ea76cf3625ae045684df9fdd207484747
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-14 15:16:54 +0100

    Improve setInterval

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+const TIME = 1000;
+
 const slides = document.querySelectorAll('.carousel__slide');
-let length = slides.length;
+const length = slides.length;
 let i = 1;
 
 setInterval(function () {
     slides[i].classList.add('carousel__slide--active');
-    //slides[i-1].classList.remove('carousel__slide--active');
-    i++;
-   if (i === length) {
-       i = 0;
-   }
-}, 3000);
+    //
+    // setTimeout(
+    //     function () {
+            const slideToHide = slides[i - 1];
+            const lastSlide = slides[slides.length -1];
+            if (slideToHide) {
+                slideToHide.classList.remove('carousel__slide--active');
+            }else {
+                lastSlide.classList.remove('carousel__slide--active');
+            }
+        // },
+    //     TIME
+    // )
 
+    i++;
+    if (i === length) {
+        i = 0;
+    }
+}, TIME);
+
+
+//slides[i-1].classList.remove('carousel__slide--active');
