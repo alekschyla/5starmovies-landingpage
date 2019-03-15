3acaeca0b5cd035ceb094cc12500298790d00dd8
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-15 16:24:52 +0100

    Some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const TIME = 1000;
+const TIMENEXTSLIDE = 1000;
+const TIMEOFSETTIMEOUT = 500;
 let i = 1;
 const slides = document.querySelectorAll('.carousel__slide');
 const slide1 = document.querySelector('.slide-1');
@@ -6,8 +7,6 @@ const length = slides.length;
 const carousel = document.querySelector(".hero__carousel-wrapper");
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-
-
 let currentIntervalId = null;
 
 const startInterval = () => (
@@ -15,7 +14,7 @@ const startInterval = () => (
         () => {
             nextSlide();
         },
-        TIME
+        TIMENEXTSLIDE
     )
 );
 
@@ -28,11 +27,11 @@ const nextSlide = () => {
     if (slideToHide) {
         setTimeout(function () {
             slideToHide.classList.remove('carousel__slide--active');
-        }, 500);
+        }, TIMEOFSETTIMEOUT);
     } else {
         setTimeout(function () {
             lastSlide.classList.remove('carousel__slide--active');
-        }, 500);
+        }, TIMEOFSETTIMEOUT);
     }
 
     i++;
@@ -47,8 +46,34 @@ const nextSlide = () => {
         slide1.classList.remove('carousel__slide--z-index');
     }
 };
-const prevSlide = () => {
 
+const prevSlide = () => {
+    slides[i].classList.add('carousel__slide--active');
+
+    const slideToHide = slides[i - 1];
+    const lastSlide = slides[length - 1];
+
+    if (slideToHide) {
+        setTimeout(function () {
+            slideToHide.classList.remove('carousel__slide--active');
+        }, TIMEOFSETTIMEOUT);
+    } else {
+        setTimeout(function () {
+            lastSlide.classList.remove('carousel__slide--active');
+        }, TIMEOFSETTIMEOUT);
+    }
+
+    i++;
+    if (i === length) {
+        i = 0;
+    }
+
+    if (i === length - 1) {
+        slide1.classList.add('carousel__slide--z-index');
+    }
+    if (i === 1) {
+        slide1.classList.remove('carousel__slide--z-index');
+    }
 };
 
 startInterval();
@@ -56,12 +81,10 @@ startInterval();
 prev.addEventListener("click", prevSlide);
 next.addEventListener("click", nextSlide);
 carousel.addEventListener("mouseenter", () => {
-    console.log("stop");
     clearInterval(currentIntervalId);
     currentIntervalId = null;
 });
 carousel.addEventListener("mouseleave", () => {
-    console.log("start");
     if (!currentIntervalId) {
         startInterval();
     }
