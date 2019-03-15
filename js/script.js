432abc5f3eed944489087fcc2522b6257e4b00d0
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-15 13:48:04 +0100

    Add text on carousel.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const TIME = 3000;
-
+const TIME = 5000;
 const slides = document.querySelectorAll('.carousel__slide');
 const slide1 = document.querySelector('.slide-1');
 const length = slides.length;
 let i = 1;
 
-setInterval(function () {
+setInterval(() => {
     slides[i].classList.add('carousel__slide--active');
 
     const slideToHide = slides[i - 1];
     const lastSlide = slides[length - 1];
+
     if (slideToHide) {
         setTimeout(function () {
             slideToHide.classList.remove('carousel__slide--active');
@@ -24,6 +24,7 @@ setInterval(function () {
     if (i === length) {
         i = 0;
     }
+
     if (i === length - 1) {
         slide1.classList.add('carousel__slide--z-index');
     }
