036a6edfdde5dd8daf79ed62eea7f4221b1d81e2
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-21 15:54:53 +0100

    Added empty methods to prototype.

--- scripts/minigame.js
+++ scripts/minigame.js
@@ -3,3 +3,31 @@ function Game(selector) {
 
     this.gameArray = [];
 }
+
+Game.prototype.render = function() {
+
+};
+
+Game.prototype.oscarMove = function () {
+
+};
+
+Game.prototype.handMove = function () {
+
+};
+
+Game.prototype.checkIfOscarWasCatched = function () {
+
+};
+
+Game.prototype.renderGameBoard = function () {
+
+};
+
+Game.prototype.endGame = function () {
+
+};
+
+Game.prototype.setInterval = function () {
+
+};
