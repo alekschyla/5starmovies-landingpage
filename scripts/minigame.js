b827f5dc963d7bf4c89c1edcee23c2c4b0abc930
Paulina Oster <paulina.oster@gmail.com>
2019-03-21 17:03:11 +0100

    initial commit

--- scripts/minigame.js
+++ scripts/minigame.js
@@ -1,7 +1,16 @@
 function Game(selector) {
     this.container = document.querySelector(selector);
-
     this.gameArray = [];
+    this.gameBoard = null;
+    this.handPosition = [
+        {x: 9, y: 19},
+        {x: 10, y: 19}
+    ];
+    this.init();
+}
+
+Game.prototype.init = function () {
+
 }
 
 Game.prototype.render = function() {
@@ -9,6 +18,12 @@ Game.prototype.render = function() {
 };
 
 Game.prototype.oscarMove = function () {
+const random = Math.ceil(Math.random() * 19);
+console.log(random);
+const oscars = new Array(20).fill(1).map(el => el = {x: random, y: -1});
+console.log(oscars);
+oscars[0].y += 1;
+console.log(oscars);
 
 };
 
