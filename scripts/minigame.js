0b83fc72c09b0260cc6cb5a844df8bb998b20d4e
Paulina Oster <paulina.oster@gmail.com>
2019-03-22 16:54:40 +0100

    added oscarsArray and oscarsMove method

--- scripts/minigame.js
+++ scripts/minigame.js
@@ -1,10 +1,12 @@
 function Game(selector) {
     this.container = document.querySelector(selector);
     this.gameArray = [];
+    this.oscars = [];
+    this.gameTick = 3000;
     this.gameBoard = null;
     this.handPosition = [
-        {x: 9, y: 19},
-        {x: 10, y: 19}
+        { x: 9, y: 19 },
+        { x: 10, y: 19 }
     ];
     this.init();
 }
@@ -13,22 +15,35 @@ Game.prototype.init = function () {
 
 }
 
-Game.prototype.render = function() {
+Game.prototype.render = function () {
 
 };
 
-Game.prototype.oscarMove = function () {
-const random = Math.ceil(Math.random() * 19);
-console.log(random);
-const oscars = new Array(20).fill(1).map(el => el = {x: random, y: -1});
-console.log(oscars);
-oscars[0].y += 1;
-console.log(oscars);
-
+Game.prototype.oscarsArray = function () {
+    this.oscars = (new Array(20))
+        .fill(1)
+        .map(el => ({
+            x: Math.floor(Math.random() * 20),
+            y: -1,
+            delay: 0
+        }))
+        for (let i = 0; i < this.oscars.length; i++){}
+    return this.oscars
 };
+
+Game.prototype.oscarsMove = function () {
+    this.oscars = this.oscarsArray();
+    for (let i = 0; i < this.oscars.length; i++) {
+        this.oscars[i].y = this.oscars[i].y + 1 + this.oscars[i].delay;
+    }
+    return this.oscars;
+}
+
+Game.prototype.oscarsInterval = function () {
+    setInterval(this.oscarsMove(), this.gameTick);
+}
 
 Game.prototype.handMove = function () {
-
 };
 
 Game.prototype.checkIfOscarWasCatched = function () {
