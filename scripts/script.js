6e4562d6db28501abaee434556b07b641f338c3f
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-27 15:57:45 +0100

    Start animation on view

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
-// document.getElementById('submit-contact').onclick=alert('Wiadomość wysłana!');
-document.getElementById('hero-button').onclick=document.getElementById('popup-window').style.display='block';
+const teammatesContainer = document.querySelector('.teammates');
+const teammatesPhotos = document.querySelectorAll('.teammate-item__photo');
+const teammatesContainerHeight = teammatesContainer.clientHeight;
+
+function inView() {
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY || window.pageYOffset;
+    const scrollPosition = scrollY + windowHeight;
+    // get element position (distance from the top of the page to the bottom of the element)
+    const elementPosition = teammatesContainer.getBoundingClientRect().top + scrollY + teammatesContainerHeight;
+    if (scrollPosition > elementPosition) {
+        return true;
+    }
+
+    return false;
+}
+
+function animate() {
+    if (inView()) {
+        teammatesPhotos.forEach(photo => photo.classList.add('teammates-animate'))
+    }
+}
+
+document.addEventListener('scroll', animate);
