31805f97937d147f5abcdcb125a03b79c009b031
Amazing Design <github@amazingdesign.eu>
2019-03-27 15:39:00 +0100

    WIP

--- js/script.js
+++ js/script.js
@@ -1,105 +1,101 @@
-const TIMENEXTSLIDE = 1000;
-const TIMEOFSETTIMEOUT = 500;
-let i = 1;
-const slides = document.querySelectorAll('.carousel__slide');
-const slide1 = document.querySelector('.slide-1');
-const lastSlide = document.querySelector('.last-slide');
-const length = slides.length;
-const carousel = document.querySelector(".hero__carousel-wrapper");
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-let currentIntervalId = null;
-let canIClick = true;
+const TIMENEXTSLIDE = 1000
+const TIMEOFSETTIMEOUT = 500
+
+const slides = document.querySelectorAll('.carousel__slide')
+let prevSlideIndex = slides.length - 1
+let currentSlideIndex = 0
+
+let currentIntervalId = null
+let canIClick = true
+
+const updateDisplay = () => {
+  console.log(prevSlideIndex, currentSlideIndex)
+
+  const prevSlide = slides[prevSlideIndex]
+  const currentSlide = slides[currentSlideIndex]
+
+  makeSlideInActive(prevSlide)
+  makeSlideActive(currentSlide)
+}
+
+const makeSlideInActive = (slide) => {
+  // slide.style.zIndex = 0
+  // setInterval(
+  //   () => {
+  //     delete slide.style.zIndex
+      slide.classList.remove('carousel__slide--active')
+    // },
+    // TIMENEXTSLIDE
+  // )
+}
+const makeSlideActive = (slide) => {
+  slide.classList.add('carousel__slide--active')
+}
 
 const startInterval = () => (
-    currentIntervalId = setInterval(
-        () => {
-            nextSlide();
-        },
-        TIMENEXTSLIDE
-    )
-);
+  currentIntervalId = setInterval(
+    () => {
+      nextSlide()
+    },
+    TIMENEXTSLIDE
+  )
+)
 
 const nextSlide = () => {
-    slides[i].classList.add('carousel__slide--active');
+  prevSlideIndex = currentSlideIndex
 
-    const slideToHide = slides[i - 1];
-    const lastSlide = slides[length - 1];
+  const nextSlide = currentSlideIndex + 1
+  if (nextSlide > slides.length - 1) {
+    currentSlideIndex = 0
+  } else {
+    currentSlideIndex = nextSlide
+  }
 
-    if (slideToHide) {
-        setTimeout(function () {
-            slideToHide.classList.remove('carousel__slide--active');
-        }, TIMEOFSETTIMEOUT);
-    } else {
-        setTimeout(function () {
-            lastSlide.classList.remove('carousel__slide--active');
-        }, TIMEOFSETTIMEOUT);
-    }
-
-    i++;
-
-    if (i === length) {
-        i = 0;
-    }
-    if (i === length - 1) {
-        slide1.classList.add('carousel__slide--z-index');
-    }
-    if (i === 1) {
-        slide1.classList.remove('carousel__slide--z-index');
-    }
-
-};
+  updateDisplay()
+}
 
 const prevSlide = () => {
-    if (i === 0) {
-        slides[length - 2].classList.add('carousel__slide--active');
-        setTimeout(
-            () => lastSlide.classList.remove('carousel__slide--active'),
-            TIMEOFSETTIMEOUT
-        )
-        i = length - 1;
-    } else if (i === 1) {
-        slides[length - 1].classList.add('carousel__slide--active');
-        setTimeout(
-            () => slides[i - 1].classList.remove('carousel__slide--active'),
-            TIMEOFSETTIMEOUT
-        )
-        i--;
-    } else {
-        slides[i - 2].classList.add('carousel__slide--active');
-        setTimeout(
-            () => slides[i - 1].classList.remove('carousel__slide--active'),
-            TIMEOFSETTIMEOUT
-        )
-        i--;
-    }
-};
+  prevSlideIndex = currentSlideIndex
 
-startInterval();
+  const prevSlide = currentSlideIndex - 1
+  if (prevSlide < 0) {
+    currentSlideIndex = slides.length - 1
+  } else {
+    currentSlideIndex = prevSlide
+  }
 
-prev.addEventListener("click", () => {
-    if (canIClick) {
-        prevSlide();
-        canIClick = false;
-        setTimeout(() => canIClick = true, 1000);
-    }
-});
+  updateDisplay()
+}
 
-next.addEventListener("click", () => {
-    if (canIClick) {
-        nextSlide();
-        canIClick = false;
-        setTimeout(() => canIClick = true, 1000);
-    }
-});
+// CONTROLS
 
-carousel.addEventListener("mouseenter", () => {
-    clearInterval(currentIntervalId);
-    currentIntervalId = null;
-});
+document.querySelector('.prev').addEventListener("click", () => {
+  if (canIClick) {
+    prevSlide()
+    canIClick = false
+    setTimeout(() => canIClick = true, 1000)
+  }
+})
 
-carousel.addEventListener("mouseleave", () => {
-    if (!currentIntervalId) {
-        startInterval();
-    }
-});
+document.querySelector('.next').addEventListener("click", () => {
+  if (canIClick) {
+    nextSlide()
+    canIClick = false
+    setTimeout(() => canIClick = true, 1000)
+  }
+})
+
+document.querySelector(".hero__carousel-wrapper").addEventListener("mouseenter", () => {
+  clearInterval(currentIntervalId)
+  currentIntervalId = null
+})
+
+document.querySelector(".hero__carousel-wrapper").addEventListener("mouseleave", () => {
+  if (!currentIntervalId) {
+    startInterval()
+  }
+})
+
+// AUTO SLIDES
+
+startInterval()
