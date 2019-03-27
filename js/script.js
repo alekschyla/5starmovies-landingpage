a381a6c1e11a89a5172684d05b126e89ecb47c92
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-27 16:14:18 +0100

    Code refactor

--- js/script.js
+++ js/script.js
@@ -1,109 +1,109 @@
-const TIMENEXTSLIDE = 1000
+const TIMENEXTSLIDE = 1000;
 
-const slides = document.querySelectorAll('.carousel__slide')
-let prevSlideIndex = slides.length - 1
-let currentSlideIndex = 0
+const slides = document.querySelectorAll('.carousel__slide');
+let prevSlideIndex = slides.length - 1;
+let currentSlideIndex = 0;
 
-let currentIntervalId = null
-let canIClick = true
+let currentIntervalId = null;
+let canIClick = true;
 
 const clearSlides = () => {
   slides.forEach(
     slide => {
-      slide.classList.remove('carousel__slide--active')
-      slide.classList.remove('carousel__slide--preserved')
+      slide.classList.remove('carousel__slide--active');
+      slide.classList.remove('carousel__slide--preserved');
     }
-  )
-}
+  );
+};
 
 const updateDisplay = () => {
-  const prevSlide = slides[prevSlideIndex]
-  const currentSlide = slides[currentSlideIndex]
+  const prevSlide = slides[prevSlideIndex];
+  const currentSlide = slides[currentSlideIndex];
 
-  clearSlides()
+  clearSlides();
 
-  preserveSlide(prevSlide)
-  makeSlideActive(currentSlide)
-}
+  preserveSlide(prevSlide);
+  makeSlideActive(currentSlide);
+};
 
 const preserveSlide = (slide) => {
-  slide.classList.add('carousel__slide--preserved')
+  slide.classList.add('carousel__slide--preserved');
   setTimeout(
     () => {
-      slide.classList.remove('carousel__slide--preserved')
+      slide.classList.remove('carousel__slide--preserved');
     },
     TIMENEXTSLIDE
-  )
-}
+  );
+};
 
 const makeSlideActive = (slide) => {
-  slide.classList.add('carousel__slide--active')
-}
+  slide.classList.add('carousel__slide--active');
+};
 
 const startInterval = () => {
   currentIntervalId = setInterval(
     () => {
-      nextSlide()
+      nextSlide();
     },
     TIMENEXTSLIDE
-  )
-}
+  );
+};
 
 const nextSlide = () => {
-  prevSlideIndex = currentSlideIndex
+  prevSlideIndex = currentSlideIndex;
 
-  const nextSlide = currentSlideIndex + 1
+  const nextSlide = currentSlideIndex + 1;
   if (nextSlide > slides.length - 1) {
-    currentSlideIndex = 0
+    currentSlideIndex = 0;
   } else {
-    currentSlideIndex = nextSlide
+    currentSlideIndex = nextSlide;
   }
 
-  updateDisplay()
-}
+  updateDisplay();
+};
 
 const prevSlide = () => {
-  prevSlideIndex = currentSlideIndex
+  prevSlideIndex = currentSlideIndex;
 
-  const prevSlide = currentSlideIndex - 1
+  const prevSlide = currentSlideIndex - 1;
   if (prevSlide < 0) {
-    currentSlideIndex = slides.length - 1
+    currentSlideIndex = slides.length - 1;
   } else {
-    currentSlideIndex = prevSlide
+    currentSlideIndex = prevSlide;
   }
 
-  updateDisplay()
-}
+  updateDisplay();
+};
 
 // CONTROLS
 
 document.querySelector('.prev').addEventListener("click", () => {
   if (canIClick) {
-    prevSlide()
-    canIClick = false
+    prevSlide();
+    canIClick = false;
     setTimeout(() => canIClick = true, TIMENEXTSLIDE)
   }
-})
+});
 
 document.querySelector('.next').addEventListener("click", () => {
   if (canIClick) {
-    nextSlide()
-    canIClick = false
-    setTimeout(() => canIClick = true, TIMENEXTSLIDE)
+    nextSlide();
+    canIClick = false;
+    setTimeout(() => canIClick = true, TIMENEXTSLIDE);
   }
-})
+});
 
 document.querySelector(".hero__carousel-wrapper").addEventListener("mouseenter", () => {
-  clearInterval(currentIntervalId)
-  currentIntervalId = null
-})
+  clearInterval(currentIntervalId);
+  currentIntervalId = null;
+});
 
 document.querySelector(".hero__carousel-wrapper").addEventListener("mouseleave", () => {
   if (!currentIntervalId) {
-    startInterval()
+    startInterval();
   }
-})
+});
 
 // AUTO SLIDES
 
-startInterval()
+startInterval();
