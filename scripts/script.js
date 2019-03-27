a96f0f62bf0fa28e6ef0d49dfc309da9f48d78a3
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-27 16:40:14 +0100

    Finish images animation

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,8 @@ function inView() {
     const windowHeight = window.innerHeight;
     const scrollY = window.scrollY || window.pageYOffset;
     const scrollPosition = scrollY + windowHeight;
-    // get element position (distance from the top of the page to the bottom of the element)
     const elementPosition = teammatesContainer.getBoundingClientRect().top + scrollY + teammatesContainerHeight;
-    if (scrollPosition > elementPosition) {
-        return true;
-    }
-
-    return false;
+    return scrollPosition > elementPosition;
 }
 
 function animate() {
