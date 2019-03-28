6aad1e844b72f83e6cf62b549c6309279052ee4f
PrzemekGdula <przemo.g12@wp.pl>
2019-03-28 15:31:01 +0100

    highlight menu element working, styles to be applied

--- scripts/script.js
+++
@@ -1,2 +0,0 @@
-// document.getElementById('submit-contact').onclick=alert('Wiadomość wysłana!');
-document.getElementById('hero-button').onclick=document.getElementById('popup-window').style.display='block';
